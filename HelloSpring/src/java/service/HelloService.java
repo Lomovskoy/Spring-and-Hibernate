26dc79a37cde5f9b77e184391f06854ffff69a2c
Lomovskoy <lomovskoy.kirill@yandex.ru>
2018-01-11 21:43:19 +0200

    Первое прилодение на спринг почему то не запустилось

---
+++ HelloSpring/src/java/service/HelloService.java
@@ -0,0 +1,11 @@
+package service;
+/**
+ * Класс службы приветствия пользователя
+ * @author imxo
+ */
+public class HelloService {
+    
+    public static String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
